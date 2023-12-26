42877b62165880ed9913c776f69524c5daa473f3
WillJcmCode54 <william199940@gmail.com>
2023-12-26 00:38:49 -0400

    Primer commit|instalacion de template AdminLTE y inicio de session

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,31 @@
+
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+<h1>Dashboard</h1>
+@stop
+
+@section('content')
+<p>Welcome to this beautiful admin panel.</p>
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="p-6 text-gray-900">
+        {{ __("You're logged in!") }}
+    </div>
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
